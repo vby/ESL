9a20f9f42584bae12c53aa54f50b914ef74964cc
vby <vbyezz@gmail.com>
2018-01-27 20:03:51 +0800

    change test framework to googletest

--- tests/span_tests.cpp
+++ tests/span_tests.cpp
@@ -1,24 +1,23 @@
 
-#define CATCH_CONFIG_MAIN
-#include <catch/catch.hpp>
+#include <gtest/gtest.h>
 #include <esl/span.hpp>
 
 //#define ESL_COMPILATION_ERROR
 
-TEST_CASE("default_constructor") {
+TEST(SpanTest, default_constructor) {
 	{
 		esl::span<int> s;
-		CHECK((s.size() == 0 && s.empty()));
+		ASSERT_TRUE((s.size() == 0 && s.empty()));
 
 		esl::span<const int> cs;
-		CHECK((cs.size() == 0 && cs.empty()));
+		ASSERT_TRUE((cs.size() == 0 && cs.empty()));
 	}
 	{
 		esl::span<int, 0> s;
-		CHECK((s.size() == 0 && s.empty()));
+		ASSERT_TRUE((s.size() == 0 && s.empty()));
 
 		esl::span<const int, 0> cs;
-		CHECK((cs.size() == 0 && cs.empty()));
+		ASSERT_TRUE((cs.size() == 0 && cs.empty()));
 	}
 #ifdef ESL_COMPILATION_ERROR
 	{
@@ -28,7 +27,7 @@ TEST_CASE("default_constructor") {
 #endif
 }
 
-TEST_CASE("constructor") {
+TEST(SpanTest, constructor) {
 	int arr[100];
 #ifdef ESL_COMPILATION_ERROR
 	{
@@ -37,23 +36,23 @@ TEST_CASE("constructor") {
 #endif
 	{
 		esl::span<int> s(arr, 0);
-		CHECK((s.size() == 0 && s.data() == arr && s.empty()));
+		ASSERT_TRUE((s.size() == 0 && s.data() == arr && s.empty()));
 	}
 	{
 		esl::span<int> s(arr, 5);
-		CHECK((s.size() == 5 && s.data() == arr && !s.empty()));
+		ASSERT_TRUE((s.size() == 5 && s.data() == arr && !s.empty()));
 	}
 }
 
-TEST_CASE("fixed_span_constructor") {
+TEST(SpanTest, fixed_span_constructor) {
 	int arr[100];
 	{
 		esl::span<int, 0> s(arr);
-		CHECK((s.size() == 0 && s.data() == arr && s.empty()));
+		ASSERT_TRUE((s.size() == 0 && s.data() == arr && s.empty()));
 	}
 	{
 		esl::span<int, 5> s(arr);
-		CHECK((s.size() == 5 && s.data() == arr && !s.empty()));
+		ASSERT_TRUE((s.size() == 5 && s.data() == arr && !s.empty()));
 	}
 #ifdef ESL_COMPILATION_ERROR
 	{
@@ -62,68 +61,68 @@ TEST_CASE("fixed_span_constructor") {
 #endif
 }
 
-TEST_CASE("size_optimization") {
-	CHECK(sizeof(esl::span<int>) == sizeof(int*) + sizeof(std::size_t));
-	CHECK(sizeof(esl::span<int, 0>) == sizeof(int*));
-	CHECK(sizeof(esl::span<int, 10>) == sizeof(int*));
+TEST(SpanTest, size_optimization) {
+	ASSERT_TRUE(sizeof(esl::span<int>) == sizeof(int*) + sizeof(std::size_t));
+	ASSERT_TRUE(sizeof(esl::span<int, 0>) == sizeof(int*));
+	ASSERT_TRUE(sizeof(esl::span<int, 10>) == sizeof(int*));
 }
 
 template <class Span>
 void test_span_subspan(const Span& s) {
 	{
 		auto ss = s.subspan(0);
-		CHECK(ss.size() == 100);
+		ASSERT_TRUE(ss.size() == 100);
 	}
 	{
 		auto ss = s.subspan(10);
-		CHECK(ss.size() == 90);
+		ASSERT_TRUE(ss.size() == 90);
 	}
 	{
 		auto ss = s.subspan(100);
-		CHECK(ss.size() == 0);
+		ASSERT_TRUE(ss.size() == 0);
 	}
 	{
-		CHECK_THROWS_AS(s.subspan(101), std::out_of_range);
+		ASSERT_THROW(s.subspan(101), std::out_of_range);
 	}
 	{
 		auto ss = s.subspan(0, 10);
-		CHECK(ss.size() == 10);
+		ASSERT_TRUE(ss.size() == 10);
 	}
 	{
 		auto ss = s.subspan(0, 110);
-		CHECK(ss.size() == 100);
+		ASSERT_TRUE(ss.size() == 100);
 	}
 	{
 		auto ss = s.subspan(90, 5);
-		CHECK(ss.size() == 5);
+		ASSERT_TRUE(ss.size() == 5);
 	}
 	{
 		auto ss = s.subspan(90, 10);
-		CHECK(ss.size() == 10);
+		ASSERT_TRUE(ss.size() == 10);
 	}
 	{
 		auto ss = s.subspan(90, 15);
-		CHECK(ss.size() == 10);
+		ASSERT_TRUE(ss.size() == 10);
 	}
 	{
 		auto ss = s.subspan(100, 5);
-		CHECK(ss.size() == 0);
+		ASSERT_TRUE(ss.size() == 0);
 	}
 	{
-		CHECK_THROWS_AS(s.subspan(101, 0), std::out_of_range);
+		ASSERT_THROW(s.subspan(101, 0), std::out_of_range);
 	}
 	{
-		CHECK_THROWS_AS(s.subspan(101, 10), std::out_of_range);
+		ASSERT_THROW(s.subspan(101, 10), std::out_of_range);
 	}
 }
 
-TEST_CASE("subspan") {
+TEST(SpanTest, subspan) {
 	std::array<int, 100> arr;
 	esl::span<int> s(arr.data(), arr.size());
 	test_span_subspan(s);
 }
 
-TEST_CASE("fixed_span_subspan") {
+TEST(SpanTest, fixed_span_subspan) {
 	std::array<int, 100> arr;
 	esl::span<int, 100> s(arr.data());
 	test_span_subspan(s);
